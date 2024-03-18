541dbdfd38e7c94e2b1a2eddd44ae19bd571661d
yasirhussainccl <yasir.hussain@ccl.com>
2024-03-18 12:45:51 +0500

    added thymleaf, fixed controller path and change server port from 8080 to 80.

--- src/main/java/com/ccl/azuredemo/TestController.java
+++ src/main/java/com/ccl/azuredemo/TestController.java
@@ -3,12 +3,12 @@ package com.ccl.azuredemo;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/test")
 public class TestController {
-
+    @GetMapping("/test")
     public String getTestPage(Model model) {
         return "index";
     }
