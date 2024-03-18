5bef6a91d28e010acf82f79fe49fed889e022bac
yasirhussainccl <yasir.hussain@ccl.com>
2024-03-18 12:34:02 +0500

    add path to test controller.

--- src/main/java/com/ccl/azuredemo/TestController.java
+++ src/main/java/com/ccl/azuredemo/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
-@RequestMapping("")
+@RequestMapping("/test")
 public class TestController {
 
     public String getTestPage(Model model) {
